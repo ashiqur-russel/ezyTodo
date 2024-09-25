07a76e78d69f34ff7f05f62f5fc4b57774693fec
Ashiqur <ashiqur.rahman@bidi.one>
2024-09-26 00:43:12 +0200

    Add footer, Hero and styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 import './App.css'
+import Footer from './Footer'
 import Header from './Header'
+import Hero from './Hero'
 function App() {
 
   return (<>
     <Header />
-    <h1>Welcome To eZyToDo</h1>
+    <div className="flex flex-col justify-center items-center">
+      <Hero />
+    </div>
+    <Footer />
   </>)
 }
 
