09c2f870129648dfeb61320cb27cb53cd8e5d18f
Ashiqur <ashiqur.rahman@bidi.one>
2024-09-28 23:25:30 +0200

    Add TaskBoard component and TaskList

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import './App.css'
 import Footer from './Footer'
 import Header from './Header'
 import Hero from './Hero'
+import TaskBoard from './task/TaskBoard'
 function App() {
 
   return (<>
@@ -9,6 +10,7 @@ function App() {
     <div className="flex flex-col justify-center items-center">
       <Hero />
     </div>
+    <TaskBoard />
     <Footer />
   </>)
 }
