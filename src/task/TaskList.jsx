b233aa2a2c34cc81549f4e53d40a1b5f1e31c8e6
Ashiqur <ashiqur.rahman@bidi.one>
2024-09-29 00:47:20 +0200

    Add default task

--- src/task/TaskList.jsx
+++ src/task/TaskList.jsx
@@ -1,5 +1,6 @@
+import { FaStar } from "react-icons/fa";
 
-export default function TaskList() {
+export default function TaskList({ tasks }) {
     return (
         <div className="overflow-auto">
             <table className="table-fixed overflow-auto xl:w-full">
@@ -29,6 +30,51 @@ export default function TaskList() {
                     </tr>
                 </thead>
                 <tbody>
+                    {tasks.map((task) => (
+                        <tr
+                            key={task.id}
+                            className="border-b border-[#2E3443] [&>td]:align-baseline [&>td]:px-4 [&>td]:py-2">
+                            <td>
+                                <button>
+                                    {task.isFavorite ? (
+                                        <FaStar color="yellow" />
+                                    ) : (
+                                        <FaStar color="gray" />
+                                    )}
+                                </button>
+                            </td>
+                            <td>{task.title}</td>
+                            <td>
+                                <div>{task.description}</div>
+                            </td>
+                            <td>
+                                <ul className="flex justify-center gap-1.5 flex-wrap">
+                                    {task.tags.map((tag) => (
+                                        <li key={tag}>
+                                            <span className="inline-block h-5 whitespace-nowrap rounded-[45px] bg-[#00D991A1] px-2.5 text-sm capitalize text-[#F4F5F6]">
+                                                {tag}
+                                            </span>
+                                        </li>
+                                    ))}
+                                </ul>
+                            </td>
+                            <td className="text-center">{task.priority}</td>
+                            <td>
+                                <div className="flex items-center justify-center space-x-3">
+                                    <button
+                                        className="text-red-500"
+                                    >
+                                        Delete
+                                    </button>
+                                    <button
+                                        className="text-blue-500"
+                                    >
+                                        Edit
+                                    </button>
+                                </div>
+                            </td>
+                        </tr>
+                    ))}
                 </tbody>
             </table>
         </div>
