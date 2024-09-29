0228c123b3e2ca64963f0568b57fa011c04de534
Ashiqur <ashiqur.rahman@bidi.one>
2024-09-29 23:33:11 +0200

    Fix alignment

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,8 +1,8 @@
 import ezyLogo from './assets/ezy.png';
 export default function Header() {
     return (
-        <nav className="flex py-6 fixed top-0 w-full !bg-[#191D26] z-50">
-            <div className=" ml-20 flex items-center justify-between gap-x-6">
+        <nav className="py-6 md:py-8 fixed top-0 w-full !bg-[#191D26] z-50">
+            <div className="container mx-auto flex items-center justify-between gap-x-6">
                 <a href="/">
                     <img
                         className="h-[45px]"
